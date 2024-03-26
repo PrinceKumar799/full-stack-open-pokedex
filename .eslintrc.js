3667686e2a470dde26178cd70f097a70522a1d44
PrinceKumar799 <77796524+PrinceKumar799@users.noreply.github.com>
2024-03-26 12:56:12 +0530

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable semi */
 /* eslint-disable indent */
 /* eslint-disable quotes */
 module.exports = {
@@ -19,13 +20,12 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    // semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
     "arrow-spacing": ["error", { before: true, after: true }],
     "react/prop-types": 0,
-    "no-console": 1,
+    "no-console": 0,
   },
 };
